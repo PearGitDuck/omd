1f77b13dcd6e09ede2dcd70079cb4640f44fd546
PearGitDuck <64581258+PearGitDuck@users.noreply.github.com>
2022-11-05 18:24:58 +0300

    Update dz_7_Advert.py

--- dz_7_Advert.py
+++ dz_7_Advert.py
@@ -1,40 +1,60 @@
 import json
+import keyword
 
 
 class Converter:
     def __init__(self, json):
         for key, value in json.items():
+            if isinstance(value, dict):
+                value = Converter(value)
+
             self.__setattr__(key, value)
 
     def __str__(self):
         return str(self.__dict__)
 
+    def __repr__(self):
+        return str(self.__dict__)
+
 
 class ColorizeMixin:
 
-    def __repr__(self, repr_color_code):
-        return f'\033[1;{repr_color_code};40m'
+    def __repr__(self):
+        text_to_mod = super().__repr__()
+        return f'\033[1;{self.repr_color_code};40m{text_to_mod}'
 
 
-class Advert(ColorizeMixin):
+class BaseAdvert:
     repr_color_code = 33
 
     def __init__(self, json):
+
         for key, value in json.items():
+            if keyword.iskeyword(key):
+                key += '_'
+            if isinstance(value, dict):
+                value = Converter(value)
             self.__setattr__(key, value)
 
-    def __setattr__(self, key, value):
-        if key == 'price' and value < 0:
-            raise ValueError('price must be >= 0')
-        if key == 'location':
-            value = Converter(value)
-        super().__setattr__(key, value)
+        if 'title' not in self.__dict__:
+            raise ValueError('Нет атрибута title')
 
-    def __getattribute__(self, item):
-        if item == 'price' and item not in self.__dict__:
+    @property
+    def price(self):
+        if '_price' not in self.__dict__:
             return 0
-        return super().__getattribute__(item)
+        return self._price
 
-        def __repr__(self):
-        return super().__repr__(Advert.repr_color_code)\
-               + f'{self.title} | {self.price}₽'
+    @price.setter
+    def price(self, new_price):
+        if new_price < 0:
+            raise ValueError('price must be >= 0')
+        else:
+            self._price = new_price
+
+    def __repr__(self):
+        return f'{self.title} | {self.price}₽'
+
+
+class Advert(ColorizeMixin, BaseAdvert):
+    pass
